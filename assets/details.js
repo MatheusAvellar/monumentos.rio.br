10e5dc59b4f7e742bc4b7576fc107aa7eab51b9a
Matheus Avellar <MatheusAvellar@users.noreply.github.com>
2023-05-21 03:00:58 -0300

    Redesign da home para ser mais amigável

--- assets/details.js
+++ assets/details.js
@@ -1,10 +1,9 @@
 // Lista de <details> in <nav>
-const details = [...document.querySelectorAll("nav > ul > li > details")];
+const details = [...document.querySelectorAll("header > nav > details")];
 const main_footer = [
 	document.querySelector("main"),
 	document.querySelector("footer")
 ];
-
 // Fecha todos os <details> que não sejam o `except`
 function closeDetails(except=null) {
 	details.forEach(el => {
@@ -12,22 +11,10 @@ function closeDetails(except=null) {
 		el.removeAttribute("open");
 	});
 }
-// Calcula se a largura do elemento é grande demais pra janela
-function isOutside(parent, child) {
-	const parent_rect = parent.getBoundingClientRect();
-	const child_rect = child.getBoundingClientRect();
-	return (parent_rect.x + child_rect.width > window.innerWidth);
-}
 // Cada vez que algum dos <details> é aberto...
 details.forEach(det => {
 	det.addEventListener("toggle", _ => {
-		if(det.open) {
-			// Fecha os outros
-			closeDetails(det);
-			// Verifica se ele está caindo fora da janela, e ajusta
-			const list = det.querySelector(":scope > ul");
-			list.style.right = isOutside(det, list) ? "0px" : "";
-		}
+		if(det.open) closeDetails(det); // Fecha os outros
 	});
 });
 // Caso o <main> ou o <footer> recebam foco, fecha <details> abertos
