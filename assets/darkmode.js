d2ea8e3f96ee02c2c24b5038545765cb0804006f
MatheusAvellar <MatheusAvellar@users.noreply.github.com>
2023-05-28 23:33:10 +0000

    deploy: 4c11a5d9c429ac6dcf2b226378d41cea649572c6

---
+++ assets/darkmode.js
@@ -0,0 +1,69 @@
+(function() {
+	// [Ref] github.com/Modernizr/Modernizr/blob/f1b57789747767db3eca410a7c7334f1904f4f37/feature-detects/storage/localstorage.js#L37
+	function isLocalStorageAvailable() {
+		const test = "TW9udW1lbnRvcyBkbyBSaW8h";
+		try {
+			localStorage.setItem(test, test);
+			localStorage.removeItem(test);
+			return true;
+		} catch(e) {
+			return false;
+		}
+	}
+	const canLocalStorage = isLocalStorageAvailable();
+	function toggleDarkMode(state) {
+		state = !!state;
+		console.log(`toggleDarkMode ${state}`);
+		toggle.checked = state;
+		// Liga o modo escuro
+		if(state) {
+			document.body.parentElement.classList.add("dark");
+			text.textContent = "Desligar modo escuro";
+		// Desliga o modo escuro
+		} else {
+			document.body.parentElement.classList.remove("dark");
+			text.textContent = "Ligar modo escuro";
+		}
+	}
+	function setLocalStorage(state) {
+		state = !!state;
+		console.log(`setLocalStorage ${state}`);
+		// Se podemos usar o local storage pra salvar a preferência
+		if(canLocalStorage)
+			localStorage.setItem("dark-mode", !!Number(state));
+	}
+
+	// Mostra botão de modo escuro, já que temos JS
+	const wrapper = document.getElementById("dark-mode-wrapper");
+	wrapper.parentElement.removeAttribute("hidden");
+	const text = document.getElementById("dark-mode-text");
+	// Escuta por cliques no checkbox de modo escuro
+	const toggle = document.getElementById("dark-mode-toggle");
+	toggle.addEventListener("change", function(e) {
+		toggleDarkMode(toggle.checked);
+		setLocalStorage(toggle.checked);
+	});
+	////
+	matchMedia = "matchMedia" in window ? window.matchMedia : (()=>false);
+	const prefersDarkMode = matchMedia("(prefers-color-scheme: dark)").matches;
+	console.log(`prefersDarkMode ${prefersDarkMode}`);
+	// Se podemos conferir se há configuração salva
+	if(canLocalStorage) {
+		const previous = localStorage.getItem("dark-mode");
+		// Se não existe configuração salva
+		if(previous === null) {
+			// Se o navegador possui preferência
+			if(prefersDarkMode) toggleDarkMode(true);
+			else toggleDarkMode(false);
+		// Se existe configuração salva
+		} else {
+			if(!!Number(previous)) toggleDarkMode(true);
+			else toggleDarkMode(false);
+		}
+	// Se não podemos conferir configurações salvas
+	} else {
+		// Se o navegador possui preferência
+		if(prefersDarkMode) toggleDarkMode(true);
+		else toggleDarkMode(false);
+	}
+})();
--- assets/details.js
+++ assets/details.js
@@ -1,24 +1,26 @@
-// Lista de <details> em <nav>
-const details = [...document.querySelectorAll("header > nav > details")];
-const main_footer = [
-	document.querySelector("main"),
-	document.querySelector("footer")
-];
-// Fecha todos os <details> que não sejam o `except`
-function closeDetails(except=null) {
-	details.forEach(el => {
-		if(el === except) return;
-		el.removeAttribute("open");
+(function() {
+	// Lista de <details> em <nav>
+	const details = [...document.querySelectorAll("header > nav > details")];
+	const main_footer = [
+		document.querySelector("main"),
+		document.querySelector("footer")
+	];
+	// Fecha todos os <details> que não sejam o `except`
+	function closeDetails(except=null) {
+		details.forEach(el => {
+			if(el === except) return;
+			el.removeAttribute("open");
+		});
+	}
+	// Cada vez que algum dos <details> é aberto...
+	details.forEach(det => {
+		det.addEventListener("toggle", _ => {
+			if(det.open) closeDetails(det); // Fecha os outros
+		});
 	});
-}
-// Cada vez que algum dos <details> é aberto...
-details.forEach(det => {
-	det.addEventListener("toggle", _ => {
-		if(det.open) closeDetails(det); // Fecha os outros
+	// Caso o <main> ou o <footer> recebam foco, fecha <details> abertos
+	main_footer.forEach(el => {
+		el.addEventListener("focusin", closeDetails);
+		el.addEventListener("click", closeDetails);
 	});
-});
-// Caso o <main> ou o <footer> recebam foco, fecha <details> abertos
-main_footer.forEach(el => {
-	el.addEventListener("focusin", closeDetails);
-	el.addEventListener("click", closeDetails);
-});
+})();
